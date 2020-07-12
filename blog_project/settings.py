8b00cb82269ba547c1506fc65ff9992eccfee514
devendraotari <devendra0014@gmail.com>
2020-07-12 23:14:28 +0530

    email password is removed from code and put into environment variable

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -146,8 +146,10 @@ LOGIN_URL = 'login'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'awesomedjangoblogapp@gmail.com'
-EMAIL_HOST_PASSWORD = 'qqdtfincafeplegw'
+# EMAIL_HOST_USER = 'awesomedjangoblogapp@gmail.com'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD='qqdtfincafeplegw'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 # EMAIL_HOST = 'localhost'
 EMAIL_PORT = 587
 
