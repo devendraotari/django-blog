d8af82c59a1f1e12f8192c112dafcdf5f68a8e40
devendraotari <devendra0014@gmail.com>
2020-07-12 22:46:58 +0530

    cometing with wrong database url

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -79,20 +79,20 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 '''
     this was default database setting 
 '''
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 '''
     this database setting is copied from youtube comment
 '''
-DATABASES = {
-    'default': {
-        'ENGINE': dj_database_url.config(conn_max_age=600, ssl_require=True)
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': dj_database_url.config(conn_max_age=600, ssl_require=True)
+#     }
+# }
 
 
 
