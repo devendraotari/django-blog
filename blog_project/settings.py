1995abdf56755dd7830f5c8328754851aa00a304
devendraotari <devendra0014@gmail.com>
2020-08-17 16:51:56 +0530

    adding AWS S3 support for profile pics

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -128,6 +128,13 @@ USE_L10N = True
 
 USE_TZ = True
 
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY') 
+AWS_STORAGE_BUCKET_NAME = 'django3-app-files'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
